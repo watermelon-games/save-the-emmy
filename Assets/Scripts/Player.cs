9be36d2f08eb6f949771b856e7b2782fbbe695c8
Dmytro Kuchura <dmytro.kuchura@amadeus.com>
2020-07-26 17:16:01 +0300

    Try to move

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,20 +4,90 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-
+    public Animator anim;
     public Rigidbody2D rb;
-    
-    public Vector2 movement;
+
+    public GameObject btnLeft;
+    public GameObject btnRight;
+
+    public float PosBtnLeft;
+    public float PosBtnRight;
+
+    public float run;
+
+    public bool IsMoveRight = true;
+    public bool IsMoveLeft = false;
+
+    public bool IsJump = false;
+
+    void Start()
+    {
+        PosBtnLeft = btnLeft.transform.position.y;
+        PosBtnRight = btnRight.transform.position.y;
+
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
 
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal"), rb.velocity.y);
+        
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                Move(true, true, false, false);
+            }
+            else
+            {
+                Move(true, false, false, false);
+            }
+        }
+        else
+        {
+            Move(false, false, false, false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
     }
 
-    void FixedUpdate()
+    void Move(bool move, bool isRight, bool crouch, bool jump)
     {
-        // rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        if (move)
+        {
+            anim.SetBool("IsWalk", true);
+        }
+        else
+        {
+            anim.SetBool("IsWalk", false);
+        }
+
+        if (isRight)
+        {
+            anim.SetBool("ToRight", true);
+            anim.SetBool("ToLeft", false);
+        }
+        else
+        {
+            anim.SetBool("ToRight", false);
+            anim.SetBool("ToLeft", true);
+        }
+        
+        anim.SetBool("IsJump", false);
+    }
+    
+    void Crouch()
+    {
+        Debug.Log("Crouch");
+    }
+    
+    void Jump()
+    {
+        anim.SetBool("IsJump", true);
+        rb.AddForce(transform.up * 5, ForceMode2D.Impulse);
     }
 }
---
+++ Assets/Scripts/UI/Button.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Button : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public void OnPointerDown(PointerEventData pointerEventData)
+    {
+        transform.position = new Vector2(transform.position.x, transform.position.y + 6f);
+    }
+
+    public void OnPointerUp(PointerEventData pointerEventData)
+    {
+        transform.position = new Vector2(transform.position.x, transform.position.y + -6f);
+    }
+}
