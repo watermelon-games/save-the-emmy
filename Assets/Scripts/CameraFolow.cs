63be2ba52f69099294d05895def48719d16febb5
Dmytro Kuchura <dmytro.kuchura@amadeus.com>
2020-07-31 21:13:49 +0300

    add something

---
+++ Assets/Scripts/CameraFolow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFolow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 cameraOffset;
+    public float followSpeed = 10f;
+    public float xMin = 0;
+    Vector3 velocity = Vector3.zero;
+
+    void FixedUpdate()
+    {
+        Vector3 targetPossition = target.position + cameraOffset;
+        Vector3 clampedPosition = new Vector3(Mathf.Clamp(targetPossition.x, xMin, float.MaxValue), targetPossition.y, targetPossition.z);
+        Vector3 smoothPosition = Vector3.SmoothDamp(transform.position, clampedPosition, ref velocity,
+            followSpeed * Time.fixedDeltaTime);
+    }
+}
